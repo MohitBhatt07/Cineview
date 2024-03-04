2856fddd05f570e7eceb9115304d3bddb86c4dde
Mohit Bhatt <bhattmohit869@gmail.com>
2024-03-04 20:48:35 +0530

    second commit

---
+++ Backend/src/test/java/Cineview/Cineview/CineviewApplicationTests.java
@@ -0,0 +1,13 @@
+package Cineview.Cineview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
