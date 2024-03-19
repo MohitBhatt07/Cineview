3bdb0cb379f8c005207b9cf88121bcf1035b8726
Mohit Bhatt <bhattmohit869@gmail.com>
2024-03-19 20:56:14 +0530

    review controller changes

--- Backend/src/main/java/Cineview/Cineview/ReviewController.java
+++ Backend/src/main/java/Cineview/Cineview/ReviewController.java
@@ -3,7 +3,10 @@ package Cineview.Cineview;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +18,8 @@ public class ReviewController {
   @Autowired
   ReviewService reviewService;
 
-  PostMapping
+  @PostMapping
   public ResponseEntity<Review> createReview(@RequestBody Map<String,String> payload){
-    return new ResponseEntity(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")));
+    return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
   }
 }
